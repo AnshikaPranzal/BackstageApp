6cce389ab372a8b33e5f598cfe3ba604244486fa
Anshika-Pranzal <anshika.pranzal@jupiter.money>
2021-11-28 19:29:34 +0530

    server.js refactor

--- server.js
+++ server.js
@@ -49,14 +49,7 @@ app.use('/api', ideaRoutes);
 
 const port = process.env.PORT || 8001;
 
-app.set('port', process.env.PORT || 5000);
-app.use(express.static('./client/build'));
-
 app.listen(8001, () => {
   console.log(`Listening on Port: ${port}`);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build",     
-  "index.html"));
-});
